8497a3734a7cf7c4befa02e6b47650bb08cf239a
karmakaryx <karmakaryx@gmail.com>
2024-03-17 23:19:46 +0900

    1st

---
+++ js/youtube.js
@@ -0,0 +1,10 @@
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+	new YT.Player('player', {
+		videoId: '9iqn1HhFJ6c'
+	});
+}
