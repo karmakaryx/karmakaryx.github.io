accb841b3ca7aab2c5fe067ae954ef65fd96b186
karmakaryx <karmakaryx@gmail.com>
2024-03-22 23:30:40 +0900

    2nd

--- js/main.js
+++ js/main.js
@@ -1,24 +1,10 @@
 'use strict'
 
-const floating1El = document.querySelector('.floating1');
-const floating2El = document.querySelector('.floating2');
-
-gsap.to(floating1El, 5, {
-	opacity: 1,
-	display: 'block'
-});
-gsap.to(floating2El, 10, {
-	opacity: 1,
-	display: 'block',
-	delay: 5
-});
-gsap.to(floating1El, 10, {
-	opacity: 0,
-	display: 'none',
-	delay: 20
-});
-gsap.to(floating2El, 10, {
-	opacity: 0,
-	display: 'none',
-	delay: 30
+new Swiper('.swiper-container', {
+	direction: 'vertical',
+	autoplay: {
+		delay: 7000
+	},
+	loop: true,
+	slidesPerView: 3
 });
